3a8fc5b75392bbaf7bc393739992d40e5a803d0a
GDRuvolo <GDRuvolo@fullsail.edu>
2015-01-24 00:10:49 -0700

    Added Test script to test CR script and sharing

---
+++ GameJam2015_Unity/Assets/Scripts/Ruvolo_TestScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ruvolo_TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log ("This is a test message for Chris' script and GitHub");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
