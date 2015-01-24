ccba7568a93c2dde4d81a1331a307f92ec93c7fc
Samantha Kern <samanthakern@vlan814-053-018.pioneernet.du.edu>
2015-01-23 23:54:05 -0700

    Player can now shoot

--- GameJam2015_Unity/Assets/AimedShoot.cs
+++ GameJam2015_Unity/Assets/AimedShoot.cs
@@ -5,24 +5,21 @@ public class AimedShoot : MonoBehaviour {
 
 	public float projectile_speed = 10;
 	public GameObject projectile;
-	public float amount = 10;
+	public float amount = 500;
 	
 	void Update()
 	{
 		if (Input.GetMouseButtonDown(0))
-		{
-			/*Debug.Log("Pressed left click.");
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast(ray))
-			{
-				GameObject p = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
-			}*/
+		{	
+			GameObject p = Instantiate(projectile, transform.position, transform.rotation) as GameObject;	
 			
 			Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
 			Vector3 dir = (Input.mousePosition - sp).normalized;
-			rigidbody2D.AddForce (dir * amount);
-		}
+			Debug.Log (dir + " , " + dir*amount);
+			p.rigidbody2D.AddForce(dir * amount);	
 			
+			var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+			p.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		}
 	}
-	
 }
