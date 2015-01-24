96372657ae7448bc12b57f88faf2c1153f046ed3
Samantha Kern <samanthakern@vlan815-056-062.pioneernet.du.edu>
2015-01-23 21:38:57 -0700

    There are now exactly one more horse(s)

---
+++ GameJam2015_Unity/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public Vector2 speed = new Vector2(10, 10);
+	
+	private Vector2 movement;
+	
+	void Update()
+	{
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+	}
+	
+	void FixedUpdate()
+	{
+		rigidbody2D.velocity = movement;
+	}
+}
