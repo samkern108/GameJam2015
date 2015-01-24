ebfc084dbaee9f4eb349d897cd1c8bd018859875
CeruleanTango <thomasdivel@gmail.com>
2015-01-24 01:12:33 -0700

    Enemy Spawner Update
    Added in new Enemy prefab along with Enemy Spawner prefab

---
+++ GameJam2015_Unity/Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Made by Thomas
+
+public class EnemyDeath : MonoBehaviour {
+
+	void OnEnable(){
+		Invoke("Destroy", 2f);
+	}
+	
+	
+	void Destroy(){
+		gameObject.SetActive(false);
+	}
+	
+	void OnDisable(){
+		CancelInvoke();
+	}
+}
