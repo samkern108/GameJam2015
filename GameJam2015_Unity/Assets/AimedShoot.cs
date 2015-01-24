18c1f87e228d0b0af1f40c5f8f05c07326f04374
Samantha Kern <samanthakern@vlan814-053-018.pioneernet.du.edu>
2015-01-23 23:26:05 -0700

    The player is still a horse but is also now a prefab

--- GameJam2015_Unity/Assets/AimedShoot.cs
+++ GameJam2015_Unity/Assets/AimedShoot.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class AimedShoot : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public float projectile_speed = 10;
+	public GameObject projectile;
+	public float amount = 10;
 	
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			/*Debug.Log("Pressed left click.");
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray))
+			{
+				GameObject p = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+			}*/
+			
+			Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+			Vector3 dir = (Input.mousePosition - sp).normalized;
+			rigidbody2D.AddForce (dir * amount);
+		}
+			
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
