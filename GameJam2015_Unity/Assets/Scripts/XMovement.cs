1512880232a5f8da8ab2d301f8e39ef25644f8b9
GDRuvolo <GDRuvolo@fullsail.edu>
2015-01-24 02:17:58 -0700

    PlayerMovement
    Added vertical and horizontal movement. Both fluid and by “row”

---
+++ GameJam2015_Unity/Assets/Scripts/XMovement.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XMovement : MonoBehaviour {
+
+	//Relation to how far the play moves per button press.
+	[SerializeField] float increment = 10.0f;
+	[SerializeField] float speed = 10.0f;
+
+	// Timer correlating towards player movement
+	[SerializeField] float timeLeft = .5f;
+
+	// Determines the screen bounds in relation to the
+	// player object. This camera object needs to be populated
+	// in the editor (drag and drop the "Main Camera" on to
+	// this attribute in the Inpector
+	public Camera CameraObject;
+
+	// Helper vector for updating the ship's position
+	// Declated here since it is used every update / tick
+	// and we do not want to waste CPU power recreating
+	// it constantly
+	private Vector3 movement = Vector3.zero;
+
+	private bool onRails = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		onRails = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (onRails == true) 
+		{
+			timeLeft -= Time.deltaTime;
+			
+			if (Input.GetKeyDown (KeyCode.A) && timeLeft <= 0) {
+				increment += speed / 100;
+				transform.position = Vector3.Lerp (transform.position, transform.position + Vector3.left * 2, increment);
+				timeLeft = .5f;
+			}
+			
+			if (Input.GetKeyDown (KeyCode.D) && timeLeft <= 0) {
+				increment += speed / 100;
+				transform.position = Vector3.Lerp (transform.position, transform.position + Vector3.right * 2, increment);
+				timeLeft = .5f;
+			
+			}
+		} 
+		else if (onRails == false)
+		{			
+			if (Input.GetKeyDown (KeyCode.A)) 
+			{
+				increment += speed / 100;
+				transform.position = Vector3.Lerp (transform.position, transform.position + Vector3.left, 0);
+			}
+			
+			if (Input.GetKeyDown (KeyCode.D)) 
+			{
+				increment += speed / 100;
+				transform.position = Vector3.Lerp (transform.position, transform.position + Vector3.right, 0);
+			}
+		}
+		UpdatePosition ();
+
+	}
+	//Update the position of the ship based on "Horizontal" input
+	void UpdatePosition()
+	{
+		// Move the player laterally in the 'X' coordinate
+		movement.x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * increment;
+		// Apply the movement vector to the game object's postition
+		gameObject.transform.Translate (movement);
+		// Trandsform the 3D world position to a screen pixel location
+		Vector3 screenPosition = CameraObject.WorldToScreenPoint (
+			gameObject.transform.position);
+		
+		//Off screen to the right?
+		if(screenPosition.x > Screen.width)
+		{
+			// Clamp (reset to the screen's right side
+			screenPosition.x = Screen.width;
+			// Transform clamped screen position to world space and
+			// assign to player ship
+			gameObject.transform.position = 
+				CameraObject.ScreenToWorldPoint(screenPosition);
+		}
+		// Off screen to the left
+		else if (screenPosition.x < 0)
+		{
+			// Clamp (reset to the screen's left side
+			screenPosition.x = 0;
+			// Transform clamped screen position to world space and
+			// assign to player ship
+			gameObject.transform.position = 
+				CameraObject.ScreenToWorldPoint(screenPosition);
+		}
+	}
+}
---
+++ GameJam2015_Unity/Assets/Scripts/YMovement.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YMovement : MonoBehaviour {
+
+	//Relation to how far the play moves per button press.
+	[SerializeField] float increment = 10.0f;
+	[SerializeField] float speed = 10.0f;
+
+	// Timer correlating towards player movement
+	[SerializeField] float timeLeft = .5f;
+
+	// Determines the screen bounds in relation to the
+	// player object. This camera object needs to be populated
+	// in the editor (drag and drop the "Main Camera" on to
+	// this attribute in the Inpector
+	public Camera CameraObject;
+
+	// Helper vector for updating the ship's position
+	// Declated here since it is used every update / tick
+	// and we do not want to waste CPU power recreating
+	// it constantly
+	private Vector3 movement = Vector3.zero;
+
+	private bool onRails = false;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		onRails = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (onRails == true) {
+	
+						timeLeft -= Time.deltaTime;
+		
+						if (Input.GetKeyDown (KeyCode.W) && timeLeft <= 0) {
+								increment += speed / 100;
+								transform.position = Vector3.Lerp (transform.position, transform.position + Vector3.up * 2, increment);
+								timeLeft = .5f;
+						}
+						if (Input.GetKeyDown (KeyCode.S) && timeLeft <= 0) {
+								increment += speed / 100;
+								transform.position = Vector3.Lerp (transform.position, transform.position + Vector3.down * 2, increment);
+								timeLeft = .5f;
+						}
+						UpdatePosition ();
+				} else if (onRails == false) {
+						if (Input.GetKeyDown (KeyCode.W)) {
+								increment += speed / 100;
+								transform.position = Vector3.Lerp (transform.position, transform.position + Vector3.up, 0);
+						}
+						if (Input.GetKeyDown (KeyCode.S)) {
+								increment += speed / 100;
+								transform.position = Vector3.Lerp (transform.position, transform.position + Vector3.down, 0);
+						}
+			UpdatePosition2();
+				}
+
+	}
+	//Update the position of the ship based on "Horizontal" input
+	void UpdatePosition()
+	{
+		//// Move the player vertia=cally in the 'Y' coordinate
+		//movement.y = Input.GetAxisRaw("Vertical") * Time.deltaTime * increment;
+		//// Apply the movement vector to the game object's postition
+		//gameObject.transform.Translate (movement);
+		// Trandsform the 3D world position to a screen pixel location
+		Vector3 screenPosition = CameraObject.WorldToScreenPoint (
+			gameObject.transform.position);
+		
+		//Off screen to the right?
+		if(screenPosition.y > Screen.height / 2)
+		{
+			// Clamp (reset to the screen's right side
+			screenPosition.y = Screen.height / 2;
+			// Transform clamped screen position to world space and
+			// assign to player ship
+			gameObject.transform.position = 
+				CameraObject.ScreenToWorldPoint(screenPosition);
+		}
+		// Off screen to the left
+		else if (screenPosition.y < 0)
+		{
+			// Clamp (reset to the screen's left side
+			screenPosition.y = 40;
+			// Transform clamped screen position to world space and
+			// assign to player ship
+			gameObject.transform.position = 
+				CameraObject.ScreenToWorldPoint(screenPosition);
+		}
+	}
+
+	void UpdatePosition2()
+	{
+		// Move the player vertia=cally in the 'Y' coordinate
+		movement.y = Input.GetAxisRaw("Vertical") * Time.deltaTime * increment;
+		// Apply the movement vector to the game object's postition
+		gameObject.transform.Translate (movement);
+		// Trandsform the 3D world position to a screen pixel location
+		Vector3 screenPosition = CameraObject.WorldToScreenPoint (
+			gameObject.transform.position);
+		
+		//Off screen to the right?
+		if(screenPosition.y > Screen.height / 2)
+		{
+			// Clamp (reset to the screen's right side
+			screenPosition.y = Screen.height / 2;
+			// Transform clamped screen position to world space and
+			// assign to player ship
+			gameObject.transform.position = 
+				CameraObject.ScreenToWorldPoint(screenPosition);
+		}
+		// Off screen to the left
+		else if (screenPosition.y < 0)
+		{
+			// Clamp (reset to the screen's left side
+			screenPosition.y = 40;
+			// Transform clamped screen position to world space and
+			// assign to player ship
+			gameObject.transform.position = 
+				CameraObject.ScreenToWorldPoint(screenPosition);
+		}
+	}
+}
