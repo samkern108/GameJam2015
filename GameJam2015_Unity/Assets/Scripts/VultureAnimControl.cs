fa38abc6caaf1c5859e624bdd9084ed00fcce43c
CeruleanTango <thomasdivel@gmail.com>
2015-01-24 10:22:34 -0700

    Animation Stuff
    Working animation stuff

---
+++ GameJam2015_Unity/Assets/Scripts/VultureAnimControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VultureAnimControl : MonoBehaviour {
+
+	Animator anim;
+	
+	int animPath = 1;
+	//bool isMoving = true;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		//anim.SetBool ("isMoving", isMoving);
+		anim.SetInteger ("animPath", animPath);
+	}
+	
+}
